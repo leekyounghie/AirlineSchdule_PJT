7f3f0cbaf8f5feae88cce0724cd2501169a7752a
leekyounghie <asianano1@gmail.com>
2015-06-19 07:23:24 +0900

    Home Commit

--- app/src/main/java/com/starnamu/airlineschdule/fragment/GoogleMapFragment.java
+++ app/src/main/java/com/starnamu/airlineschdule/fragment/GoogleMapFragment.java
@@ -1,7 +1,6 @@
 package com.starnamu.airlineschdule.fragment;
 
 import android.content.Context;
-import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -15,9 +14,10 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.GroundOverlayOptions;
-import com.google.android.gms.maps.model.IndoorBuilding;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.starnamu.airlineschdule.animationaircraft.MakerAircraft;
 import com.starnamu.projcet.airlineschedule.R;
 
 /**
@@ -28,6 +28,7 @@ public class GoogleMapFragment extends Fragment {
     MapView mapView;
     GoogleMap map;
     LocationManager manager;
+    LatLng latLng;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,49 +41,22 @@ public class GoogleMapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.some_layout, container, false);
 
-        // Gets the MapView from the XML layout and creates it
         mapView = (MapView) v.findViewById(R.id.mapview);
         mapView.onCreate(savedInstanceState);
 
-        // Gets to GoogleMap from the MapView and does initialization stuff
+
         map = mapView.getMap();
         map.getUiSettings().setMyLocationButtonEnabled(false);
         map.setMyLocationEnabled(true);
 
-        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
-        MapsInitializer.initialize(this.getActivity());
-        // Updates the location and zoom of the MapView
+//        MapsInitializer.initialize(this.getActivity());
         CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(37.4692, 126.4406957), 12);
         map.animateCamera(cameraUpdate);
 
-        LatLng NEWARk = new LatLng(37.4692, 126.4406957);
-        GroundOverlayOptions newarMap = new GroundOverlayOptions()
-                .image(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher))
-                .position(NEWARk, 8600f, 6500f);
+        latLng = new LatLng(37.465, 126.445);
 
-        map.addGroundOverlay(newarMap);
-map.setOnIndoorStateChangeListener(new GoogleMap.OnIndoorStateChangeListener() {
-    @Override
-    public void onIndoorBuildingFocused() {
-
-    }
-
-    @Override
-    public void onIndoorLevelActivated(IndoorBuilding indoorBuilding) {
-
-    }
-});
         return v;
     }
-
-
-
-    /*LatLng NEWARK = new LatLng(40.714086, -74.228697);
-
-    GroundOverlayOptions newarkMap = new GroundOverlayOptions()
-            .image(BitmapDescriptorFactory.fromResource(R.drawable.newark_nj_1922))
-            .position(NEWARK, 8600f, 6500f);
-    map.addGroundOverlay(newarkMap);*/
 
     @Override
     public void onPause() {
@@ -96,6 +70,10 @@ map.setOnIndoorStateChangeListener(new GoogleMap.OnIndoorStateChangeListener() {
     public void onResume() {
         mapView.onResume();
         super.onResume();
+
+        Marker marker = map.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_media_play)).position(latLng));
+        MakerAircraft markerAircreaft = new MakerAircraft(marker, latLng);
+        markerAircreaft.start();
 
         //내 위치 좌표 깜빡임 시작
         map.setMyLocationEnabled(true);
