f086c815d6b7bd909330a3a1c19134dd8d3b55ad
MyeongSic <you6878@icloud.com>
2015-06-21 16:44:05 +0900

    바운스를 없애고 리니어로 집어넣은 겁니다.
    -명식

---
+++ app/src/main/java/com/starnamu/airlineschdule/GoogleMapAirCraft/AirPortPosition.java
@@ -0,0 +1,10 @@
+package com.starnamu.airlineschdule.GoogleMapAirCraft;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by MyeongSic on 2015. 6. 21..
+ */
+public class AirPortPosition {
+    static LatLng airportPosition = new LatLng(37.3713, 126.654);
+}
